9b8dea32867de8ed005fb67a647542c6e0805ce8
vagrant <vagrant@jenkins>
2019-05-24 03:23:15 +0000

    final update for test functions

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,8 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Welcome extends CI_Controller {
-
 	/**
 	 * Index Page for this controller.
 	 *
@@ -24,7 +22,6 @@ class Welcome extends CI_Controller {
 	}
 	public function index()
 	{
-
 		$this->load->view('welcome_message');
 	}
 	public function book(){
